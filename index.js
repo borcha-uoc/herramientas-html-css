aaa58b5958972aba714e7c18845507a1228a5518
Bruno Orcha García <borcha@uoc.edu>
2019-11-17 13:40:18 +0100

    Se importa los objetos hightligh.js y se registran las sintaxis usadas

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 import 'bootstrap';
 import './scss/main.scss';
+import 'highlight.js/styles/default.css';
+
+import hljs from 'highlight.js/lib/highlight';
+import coffeescript from 'highlight.js/lib/languages/coffeescript';
+import javascript from 'highlight.js/lib/languages/javascript';
 
 console.log('hola mundo')
+hljs.registerLanguage('coffeescript', coffeescript);
+hljs.registerLanguage('javascript', javascript);
+hljs.initHighlightingOnLoad();
